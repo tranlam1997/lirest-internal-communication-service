0eb6c07ce7d14022660bd0a89ce9fd6f19ad9a65
Tran Tuan Lam <foet1997@gmail.com>
2022-12-25 17:43:14 +0700

    update service client interface

--- src/interfaces/grpc.interface.ts
+++ src/interfaces/grpc.interface.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/ban-types */
-import { ChannelCredentials, ChannelOptions, Client, ServiceDefinition } from "@grpc/grpc-js";
-import { UsersServiceClient } from "./service.interface";
+import { ChannelCredentials, ChannelOptions, ServiceDefinition } from "@grpc/grpc-js";
+import * as ServiceClient from "../proto";
 
-export type LirestServiceClient = UsersServiceClient
+export type LirestServiceClient = ServiceClient.UsersServiceClient
 
 export interface LirestServiceClientConstructor {
   new(address: string, credentials: ChannelCredentials, options?: Partial<ChannelOptions>): LirestServiceClient;
--- src/interfaces/service.interface.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/ban-types */
-import { Client } from "@grpc/grpc-js";
-
-export interface UsersServiceClient extends Client {
-  getUserByEmail: Function;
-  getUserById: Function;
-  updateUser: Function;
-}
