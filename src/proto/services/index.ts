6f52f04544ab415ebf7a5c64b5c1f8e07ff4477f
Tran Tuan Lam <foet1997@gmail.com>
2022-12-27 01:26:13 +0700

    Export health service types

--- src/proto/services/index.ts
+++ src/proto/services/index.ts
@@ -1,1 +1,4 @@
-export * from './user/v1/user_service'
+import * as UserService from './user/v1/user_service';
+import * as HealthService from './health/v1/health_service';
+
+export default {...UserService, ...HealthService};
