21d421aa990314c5946e691dc02499236ce8e743
Tran Tuan Lam <foet1997@gmail.com>
2022-12-27 23:39:52 +0700

    Export lirest grpc enum and interface

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,5 @@ export * from './client';
 export * from './server';
 export * from './proto';
 export * from '@grpc/grpc-js';
+export * from './interfaces/grpc.interface';
+export * from './enums/grpc.enum';
