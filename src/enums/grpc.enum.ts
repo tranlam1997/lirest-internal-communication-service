2d0b2bc24c0be5e08070d2a41d7884b47aeedb11
Tran Tuan Lam <foet1997@gmail.com>
2022-12-28 00:55:37 +0700

    Fix glob path

--- src/enums/grpc.enum.ts
+++ src/enums/grpc.enum.ts
@@ -3,10 +3,15 @@ import glob from 'glob';
 export const ServiceVersions = {
   v1: 'v1',
 }
-const protoServices = glob.sync(('dist/proto/services/*'), { ignore: ['**/index.ts'] });
+const pathToProto = '../proto/services';
+export const protoServices = glob.sync(`${pathToProto}/*`,
+  {
+    cwd: __dirname,
+    ignore: [`${pathToProto}/**/index*.{ts,js}`]
+  });
 
 export const ServiceNames = protoServices.map((service) => {
   const serviceName = service.split('/').pop();
   // eg: services.users.v1.UsersService
   return `services.${serviceName}.${ServiceVersions.v1}.${serviceName[0].toUpperCase() + serviceName.slice(1)}Service`;
-})
+});
